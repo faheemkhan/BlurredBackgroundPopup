afe1c159c3eec5b5fdfdc5faaedc8f92e517ca3b
Faheem <faheem.gem@gmail.com>
2022-12-08 12:36:07 +0200

    first commit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const closeIconEl = document.querySelector(".close-icon");
 btnEl.addEventListener("click", ()=>{
     containerEl.classList.add("active");
     popupContainerEl.classList.remove("active");
-})
+});
 
 closeIconEl.addEventListener("click", ()=>{
     containerEl.classList.remove("active");
     popupContainerEl.classList.add("active");
 
-})
+});
